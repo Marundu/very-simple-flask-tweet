0a2feaa025078e3f17367e70d60bcc669f363b08
Marundu <marundu@gmail.com>
2016-08-12 23:33:14 +0300

    A small personal tweet app

---
+++ twitterhelper.py
@@ -0,0 +1,15 @@
+import tweepy
+import twitterconfig
+
+class TwitterAPI:
+	def __init__(self):
+		consumer_key=twitterconfig.consumer_key
+		consumer_secret=twitterconfig.consumer_secret
+		auth=tweepy.OAuthHandler(consumer_key, consumer_secret)
+		access_token=twitterconfig.access_token
+		access_token_secret=twitterconfig.access_token_secret
+		auth.set_access_token(access_token,access_token_secret)
+		self.api=tweepy.API(auth)
+	
+	def tweet(self, message):
+		self.api.update_status(status=message)
---
+++ twittertweet.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+from twitterhelper import TwitterAPI
+
+app=Flask(__name__)
+
+twitter=TwitterAPI()
+
+@app.route('/')
+@app.route('/tweet', methods=['POST'])
+def send_tweet():
+	try:
+		tweet_text=request.form.get('tweet')
+		twitter.tweet(tweet_text)
+	except Exception as e:
+		print e
+		tweet_text=None
+	return render_template('tweet.html', tweet_text=tweet_text)
+
+if __name__=='__main__':
+	app.run(debug=True, port=7099)
