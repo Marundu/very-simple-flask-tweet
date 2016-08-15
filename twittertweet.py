c675302830b4a7f623b51c64fa38c05b949b2c4e
Marundu <marundu@gmail.com>
2016-08-15 16:56:38 +0300

    Separated send_tweet() and added home()

--- twittertweet.py
+++ twittertweet.py
@@ -6,6 +6,14 @@ app=Flask(__name__)
 twitter=TwitterAPI()
 
 @app.route('/')
+def home():
+	try:
+		tweet_text=None
+	except Exception as e:
+		print e
+		tweet_text=None
+	return render_template('tweet.html', tweet_text=tweet_text)
+
 @app.route('/tweet', methods=['POST'])
 def send_tweet():
 	try:
@@ -13,8 +21,8 @@ def send_tweet():
 		twitter.tweet(tweet_text)
 	except Exception as e:
 		print e
-		tweet_text=None
-	return render_template('tweet.html', tweet_text=tweet_text)
+		#tweet_text=None
+	return home()#render_template('tweet.html', tweet_text=tweet_text)
 
 if __name__=='__main__':
 	app.run(debug=True, port=7090)
