f182d90280826628b1de8ac373e223eeddd95661
Marundu <marundu@gmail.com>
2016-10-27 17:20:11 +0300

    Added secret key to main app, prettified HTML page, added Milligram awesomeness

--- twittertweet.py
+++ twittertweet.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, flash, render_template, request
 from twitterhelper import TwitterAPI
+import os
 
 app=Flask(__name__)
+SECRET_KEY=os.urandom(24)
+
+app.secret_key=SECRET_KEY
 
 twitter=TwitterAPI()
 
@@ -19,10 +23,10 @@ def send_tweet():
 	try:
 		tweet_text=request.form.get('tweet')
 		twitter.tweet(tweet_text)
+		flash('Your tweet has been sent!')
 	except Exception as e:
 		print e
-		#tweet_text=None
-	return home()#render_template('tweet.html', tweet_text=tweet_text)
+	return home()
 
 if __name__=='__main__':
 	app.run(debug=True, port=7090)
